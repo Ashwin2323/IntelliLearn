bf991ba1346bc89bb3fa123e30966dedc8d792dd
Ashwin2323 <ashwinhammar5555@gmail.com>
2025-08-04 22:30:59 +0530

    Updated 2

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://intellilearn-7iut.onrender.com/",
+    origin: "http//:localhost:8080/",
     credentials: true
 }));
 app.use(cookieParser());
